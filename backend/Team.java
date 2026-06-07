419bae4a298e6d78cbe077d4f1401d53de214e52
2251150019 Nguyen Huu Kieu <2251150019@ut.edu.vn>
2026-06-07 21:14:07 +0700

    Create Team.java

---
+++ backend/Team.java
@@ -0,0 +1,11 @@
+public class Team {
+    private int id;
+    private String teamName;
+    private int leaderId;
+
+    public Team(int id, String teamName, int leaderId) {
+        this.id = id;
+        this.teamName = teamName;
+        this.leaderId = leaderId;
+    }
+}
