369f6938e77b2140220b2dd565cbba0312a4113e
2251150019 Nguyen Huu Kieu <2251150019@ut.edu.vn>
2026-06-07 21:38:37 +0700

    Create Competition.java

---
+++ backend/Competition.java
@@ -0,0 +1,15 @@
+public class Competition {
+    private int id;
+    private String name;
+    private String startDate;
+    private String endDate;
+    private String status;
+
+    public Competition(int id, String name, String startDate, String endDate, String status) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+}
